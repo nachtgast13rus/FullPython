3569c74f410434334275a5c19a1897e53416ba8c
nachtgast13rus <dtygushev@yandex.ru>
2020-07-30 09:31:47 +0300

    add_more_for_string

--- data_types/str/more_for_str.py
+++ data_types/str/more_for_str.py
@@ -5,5 +5,11 @@ string = 'Hello World'
 # string[0] = 'P' # return error
 string_list = list(string) # Преобразуем строку в список
 string_list[0] = 'P' # Меняем нужный элемент
-string = ''.join(string_list) # Преобразуем список в строку
-print(string)
+string_1 = ''.join(string_list) # Преобразуем список в строку
+print(string, string_1, sep='\n')
+
+# Or
+new_string = bytearray(b'Evgen')
+new_string.extend(b' is foggot')
+new_string = new_string.decode()
+print(new_string)
